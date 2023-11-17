9f43aeefd760404d8a3867eb6b616942a43ff17a
Andrey Rumm <arumm@tradingview.com>
2023-11-17 19:31:20 +0300

    Data updates

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { MouseEvent, PropsWithChildren, useEffect, useState } from 'react';
-import { MoreVert } from '@mui/icons-material';
-import { Card, CardHeader, IconButton, List, Menu, MenuItem } from '@mui/material';
+import { PropsWithChildren, useEffect, useState } from 'react';
+import { Card, CardHeader, List } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { useRouter } from 'next/navigation';
 
-import { useCopyToClipboard } from '@/hooks/use-copy-to-clipboard';
 import useLocalStorage from '@/hooks/use-local-storage';
 import { AddressInfo, GroupInfo, MessengerInfo, PhoneInfo, PropsWithStyles, WebsiteInfo } from '@/types';
 
@@ -31,14 +28,11 @@ const StyledCard = styled(Card)`
   }
 `;
 
-export const InfoCard = ({ info, singleCard = false, skipCopy = false, onCopyUrl, style }: Props) => {
-  const { id, title, subtitle, logo, addresses, phones, messengers, urls, color, rows = 1, } = info;
+export const InfoCard = ({ info, singleCard = false, style }: Props) => {
+  const { id, title, subtitle, logo, addresses, phones, messengers, urls, rows = 1, } = info;
   const [isOpenedInitially, updateSettings] = useLocalStorage<boolean>(`card_${id}`, false);
   const [isOpened, setIsOpened] = useState(singleCard);
-  const [, copy] = useCopyToClipboard();
   const [copiedState, setCopiedState] = useState<boolean>(false);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const router = useRouter();
 
   useEffect(() => {
     if (copiedState) {
@@ -60,30 +54,6 @@ export const InfoCard = ({ info, singleCard = false, skipCopy = false, onCopyUrl
     setIsOpened(prev => !prev);
   };
 
-  const handleMenu = (event: MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleShare = (e: MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setCopiedState(true);
-    copy(window.location.href + id);
-    handleClose();
-  };
-
-  const handleOpenPage = (e: MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    router.push('/' + id);
-  };
-
   const contentStyle = singleCard ? {} : { paddingTop: 0 };
   const wrapperStyle = {
     gridRow: `span ${isOpened ? rows : 1}`,
@@ -92,61 +62,41 @@ export const InfoCard = ({ info, singleCard = false, skipCopy = false, onCopyUrl
   };
 
   return (
-    <>
-      <Menu
-        anchorEl={ anchorEl }
-        open={ Boolean(anchorEl) }
-        onClose={ handleClose }
-        keepMounted
-      >
-        <MenuItem onClick={ handleShare }>Скопировать ссылку</MenuItem>
-        <MenuItem onClick={ handleOpenPage }>Открыть в отдельном окне</MenuItem>
-      </Menu>
-      <StyledCard style={ wrapperStyle } >
-        { !singleCard && (
-          <CardHeader
-            sx={ {
-              cursor: 'pointer',
-              '& .MuiCardHeader-content': {
-                display: 'block',
-                overflow: 'hidden',
-              },
-            } }
-            avatar={ logo && (<Logo alt={ title } type={ logo } />) }
-            onClick={ handleChange }
-            title={ title }
-            titleTypographyProps={ {
-              variant: 'h2',
-              fontSize: 18,
-            } }
-            subheader={ subtitle }
-            subheaderTypographyProps={ {
-              noWrap: true,
-              textOverflow: 'ellipsis',
-              variant: 'h3',
-              fontSize: 16,
-              color: 'text.secondary'
-            } }
-            action={ (
-              <IconButton onClick={ handleMenu }  >
-                <MoreVert />
-              </IconButton>
-            ) }
-          />
-        ) }
-        { isOpened && (
-          <List >
-            { addresses?.map((a: AddressInfo, i: number) => <Address onClick={ onCopyUrl } iconUrl={ skipCopy ? '' : getUrl(id, 'addresses', i) } key={ i } { ...a } />) }
-            { phones?.map((p: PhoneInfo, i: number) => <Phone onClick={ onCopyUrl } iconUrl={ skipCopy ? '' : getUrl(id, 'phones', i) } key={ i } { ...p } />) }
-            { messengers?.map((m: MessengerInfo, i: number) => <Messenger onClick={ onCopyUrl } iconUrl={ skipCopy ? '' : getUrl(id, 'messengers', i) } key={ i } { ...m } />) }
-            { urls?.map((w: WebsiteInfo, i: number) => <WebLink onClick={ onCopyUrl } iconUrl={ skipCopy ? '' : getUrl(id, 'urls', i) } key={ i } { ...w } />) }
-          </List>
-        ) }
-      </StyledCard>
-    </>
+    <StyledCard style={ wrapperStyle } >
+      { !singleCard && (
+        <CardHeader
+          sx={ {
+            cursor: 'pointer',
+            '& .MuiCardHeader-content': {
+              display: 'block',
+              overflow: 'hidden',
+            },
+          } }
+          avatar={ logo && (<Logo alt={ title } type={ logo } />) }
+          onClick={ handleChange }
+          title={ title }
+          titleTypographyProps={ {
+            variant: 'h2',
+            fontSize: 18,
+          } }
+          subheader={ subtitle }
+          subheaderTypographyProps={ {
+            noWrap: true,
+            textOverflow: 'ellipsis',
+            variant: 'h3',
+            fontSize: 16,
+            color: 'text.secondary'
+          } }
+        />
+      ) }
+      { isOpened && (
+        <List >
+          { addresses?.map((a: AddressInfo, i: number) => <Address key={ i } { ...a } />) }
+          { phones?.map((p: PhoneInfo, i: number) => <Phone key={ i } { ...p } />) }
+          { messengers?.map((m: MessengerInfo, i: number) => <Messenger key={ i } { ...m } />) }
+          { urls?.map((w: WebsiteInfo, i: number) => <WebLink key={ i } { ...w } />) }
+        </List>
+      ) }
+    </StyledCard>
   );
 };
-
-function getUrl(id: string, dataType: string, index: number) {
-  return document?.location.origin + '/' + [id, dataType, index + 1].join('/');
-}
