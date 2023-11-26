0cbf4daf0c06dd3bd8596a9e0a173fce9674fd28
Andrew Rumm <rayz@rayz.ru>
2023-11-26 20:11:45 +0300

    Updated env

--- src/app/parking/request/page.tsx
+++ src/app/parking/request/page.tsx
@@ -129,7 +129,7 @@ export default function Parking() {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    borderRadius: 8,
     boxShadow: 24,
     p: 4,
     display: 'flex',
