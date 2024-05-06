1a11562ba3f3b5836ab0105a11d0523f9d55335d
Andrew Rumm <rayz@rayz.ru>
2024-05-06 19:54:49 +0300

    Separated navigation

---
+++ src/app/rules/layout.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+
+import RootLayout from '../layout';
+
+export const metadata: Metadata = {
+  title: 'Правила сообщества',
+  description: 'Сердце Ростова 2',
+};
+
+export default RootLayout;
